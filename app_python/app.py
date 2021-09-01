5930a6f98c9531689c29eff0721092f7b477157b
lissa_m <42211844+AlisaMartyanova@users.noreply.github.com>
2021-09-01 11:55:57 +0300

    Update app.py

--- app_python/app.py
+++ app_python/app.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 def get_moscow_time():
-    time_zone = pytz.timezone('Europe/Moscow')
+    time_zone = pytz.timezone("Europe/Moscow")
     moscow_time = datetime.now(time_zone)
     return moscow_time
+
 
 @app.route("/")
 def index():
     return render_template("template.html", time=get_moscow_time().strftime("%H:%M:%S"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run('localhost', 5000)
