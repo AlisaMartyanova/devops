8dec8163413f428b7e60e843290ccb4810debb96
AlisaMartyanova <alicemartyanova48@gmail.com>
2021-09-06 20:25:05 +0300

    ansible

---
+++ terraform/dev/app_python/security_group.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "http" {
+  name        = "http"
+  description = "Allow HTTP" traffic
+
+  ingress = [
+    {
+      description      = "Http inbound"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  egress = [
+    {
+      description      = "Http outbound"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow SSH traffic"
+
+  ingress = [
+    {
+      description      = "SSH inbound"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+}
